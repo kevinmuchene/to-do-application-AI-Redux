d59ff39274b76cdbb8931a3973c864bddb7a1fbe
Kevin Muchene <muchenekevin@gmail.com>
2023-11-05 09:31:17 -0600

    Dummy data (to-dos)

---
+++ src/dummydata/todolist.ts
@@ -0,0 +1,36 @@
+export const todos = [
+  {
+    id: "1b4f18f5-ba50-4b5e-9d7e-7e7a1faaaa31",
+    title: "Grocery Shopping",
+    description: "Buy groceries for the week, including bread, milk, and eggs.",
+    dateCreated: "2023-11-05",
+  },
+  {
+    id: "2c5f16a9-ac2b-4f89-9f92-efef78f5a642",
+    title: "Finish Project Report",
+    description:
+      "Complete the final sections of the project report and send it to the team by Tuesday.",
+    dateCreated: "2023-11-05",
+  },
+  {
+    id: "3d8f1be5-c215-456b-8c1d-2b2a8d0c38ff",
+    title: "Schedule Doctor's Appointment",
+    description:
+      "Call Dr. Smith's office to schedule the annual check-up appointment.",
+    dateCreated: "2023-11-05",
+  },
+  {
+    id: "4e9d11c5-da8f-4a4e-b9b1-ee5a9b3b8a6b",
+    title: "Car Maintenance",
+    description:
+      "Take the car to the service center for scheduled maintenance and oil change.",
+    dateCreated: "2023-11-05",
+  },
+  {
+    id: "5fae15c2-d7cb-4ec1-bf65-ee8a9c5f67b3",
+    title: "Parent-Teacher Conference",
+    description:
+      "Attend the parent-teacher conference at Jimmy’s school on Thursday at 2 PM.",
+    dateCreated: "2023-11-05",
+  },
+];
