b4d489ff03a43f17d4f12e4839fc91364728bc8a
Kevin Muchene <muchenekevin@gmail.com>
2023-11-09 13:06:25 -0600

    deletetodoslice with intialstate, name and reducers

---
+++ src/components/redux/slices/deleteToDosSlice.ts
@@ -0,0 +1,28 @@
+import { ToDo } from "../../../common/interfaces/Interfaces";
+import { deletedTodos } from "../../../dummydata/todolist";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../Store";
+
+export interface DeletedToDoState {
+  deletedToDos: ToDo[];
+}
+
+const initialState: DeletedToDoState = {
+  deletedToDos: deletedTodos,
+};
+
+export const deleteToDoSlice = createSlice({
+  name: "deletedTodos",
+  initialState,
+  reducers: {
+    addDeleteToDo: (state, action: PayloadAction<ToDo>) => {
+      state.deletedToDos.unshift(action.payload);
+    },
+  },
+});
+
+export const { addDeleteToDo } = deleteToDoSlice.actions;
+export const selectDeletedTodos = (state: RootState) =>
+  state.deletedToDos.deletedToDos;
+
+export default deleteToDoSlice.reducer;
