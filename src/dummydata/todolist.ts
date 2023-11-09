27b7aae3025e32c9ee465b08402c62387e61d522
Kevin Muchene <muchenekevin@gmail.com>
2023-11-09 14:06:49 -0600

    date change

--- src/dummydata/todolist.ts
+++ src/dummydata/todolist.ts
@@ -3,70 +3,70 @@ export const todos = [
     id: "1b4f18f5-ba50-4b5e-9d7e-7e7a1faaaa31",
     title: "Grocery Shopping",
     description: "Buy groceries for the week, including bread, milk, and eggs.",
-    dateCreated: "11-052023",
+    dateCreated: "11-05-2023",
   },
   {
     id: "2c5f16a9-ac2b-4f89-9f92-efef78f5a642",
     title: "Finish Project Report",
     description:
       "Complete the final sections of the project report and send it to the team by Tuesday.",
-    dateCreated: "11-052023",
+    dateCreated: "11-05-2023",
   },
   {
     id: "3d8f1be5-c215-456b-8c1d-2b2a8d0c38ff",
     title: "Schedule Doctor's Appointment",
     description:
       "Call Dr. Smith's office to schedule the annual check-up appointment.",
-    dateCreated: "11-052023",
+    dateCreated: "11-05-2023",
   },
   {
     id: "4e9d11c5-da8f-4a4e-b9b1-ee5a9b3b8a6b",
     title: "Car Maintenance",
     description:
       "Take the car to the service center for scheduled maintenance and oil change.",
-    dateCreated: "11-052023",
+    dateCreated: "11-05-2023",
   },
   {
     id: "5fae15c2-d7cb-4ec1-bf65-ee8a9c5f67b3",
     title: "Parent-Teacher Conference",
     description:
       "Attend the parent-teacher conference at Jimmy’s school on Thursday at 2 PM.",
-    dateCreated: "11-052023",
+    dateCreated: "11-05-2023",
   },
   {
     id: "6fae15c2-d7cb-4ec1-bf65-ee8a9c5f67b3",
     title: "Update Website Content",
     description:
       "Review the latest blog drafts and update the company website with the new content by next Monday.",
-    dateCreated: "11-052023",
+    dateCreated: "11-05-2023",
   },
   {
     id: "7b1f18f5-ba50-4b5e-9d7e-7e7a1faaaa31",
     title: "Plan Birthday Party",
     description:
       "Organize a surprise birthday party for Sarah, including ordering the cake and sending out invitations.",
-    dateCreated: "11-052023",
+    dateCreated: "11-05-2023",
   },
   {
     id: "8c5f16a9-ac2b-4f89-9f92-efef78f5a642",
     title: "Gym Membership Renewal",
     description:
       "Renew gym membership and schedule sessions with the personal trainer.",
-    dateCreated: "11-052023",
+    dateCreated: "11-05-2023",
   },
   {
     id: "9d8f1be5-c215-456b-8c1d-2b2a8d0c38ff",
     title: "Book Club Meeting",
     description:
       "Read the selected book 'The Midnight Library' and prepare discussion questions for the book club meeting on November 20th.",
-    dateCreated: "11-052023",
+    dateCreated: "11-05-2023",
   },
   {
     id: "10e9d11c5-da8f-4a4e-b9b1-ee5a9b3b8a6b",
     title: "Prepare Tax Documents",
     description:
       "Gather all necessary financial documents to prepare for tax filing before the end of the year.",
-    dateCreated: "11-052023",
+    dateCreated: "11-05-2023",
   },
 ];
 
